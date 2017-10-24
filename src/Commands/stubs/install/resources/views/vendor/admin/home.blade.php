43c0e73a003b826a72b298a61fc5cb8ed3b802e8
kaiwh <958262243@qq.com>
2017-10-24 12:16:32 +0800

    update

--- src/Commands/stubs/install/resources/views/vendor/admin/home.blade.php
+++ src/Commands/stubs/install/resources/views/vendor/admin/home.blade.php
@@ -2,15 +2,6 @@
 
 @section('content')
 <div id="content">
-    <div class="container-fluid">
-        <div class="panel panel-default">
-            <div class="panel-body">
-                <a class="img-thumbnail" data-toggle="image" href="#" id="thumb-image">
-                    <img alt="" data-placeholder="{{ Image::placeholder(100,100) }}" src="{{ Image::placeholder(100,100) }}" title=""/>
-                </a>
-                <input id="input-image" name="image" type="hidden" value=""/>
-            </div>
-        </div>
-    </div>
+   
 </div>
 @endsection
